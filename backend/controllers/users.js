2c6343e1e1ab61f3bda39679a3228894144cc5bc
Brian <brian.thiely@gmail.com>
2021-10-17 23:46:05 +0200

    all routes user created

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,60 +1,152 @@
-const { response } = require('express');
 const { User } = require('../models');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const cryptojs = require('crypto-js');
 require('dotenv').config();
 
-exports.create = async (req, res, next) => {
+// Création compte utilisateur
+exports.register = async (req, res, next) => {
+	const email = req.body.email;
+	const password = req.body.password;
+	const firstName = req.body.firstName;
+	const lastName = req.body.lastName;
+	const role = req.body.role;
 
+	const emailRegex =
+		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	const passwordRegex =
+		/^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*])[\w!@#$%^&*]{8,}$/;
 
-
-	// Création user
 	try {
-		const { body } = req;
-		const responseAdded = await User.create(body);
-		res.send(responseAdded);
+		if (!email && !password && !firstname && !lastname && !role) {
+			throw new Error('Champs manquant');
+		}
+
+		if (!emailRegex.test(email)) {
+			throw new Error("Format d'email non valide");
+		}
+
+		if (!passwordRegex.test(password)) {
+			throw new Error(
+				'Au moins 8 caractères - Inclure au moins 1 lettre minuscule - 1 lettre majuscule - 1 chiffre - 1 caractère spécial'
+			);
+		}
+
+		const alreadyUser = await User.findOne({
+			attributes: ['email'],
+			where: { email: email },
+		});
+
+		if (alreadyUser) {
+			throw new Error('Vous avez déjà un compte');
+		}
+
+		// Si je peux crypter plus haut je remplace ceci par
+		// const { body } = req;
+		// 	// 	const newUser = await User.create(body);
+		const newUser = await User.create({
+			email: cryptojs
+				.HmacSHA512(req.body.email, process.env.SECRET_CRYPTOJS_TOKEN)
+				.toString(cryptojs.enc.Base64),
+			password: await bcrypt.hash(password, 10),
+			firstName: firstName,
+			lastName: lastName,
+			role: role,
+			// picture: picture,
+			// isAdmin: 0,
+		});
+
+		if (!newUser) {
+			throw new Error(
+				"Désolé, une erreur s'est produite, veuillez réessayer plus tard"
+			);
+		}
+
+		const token =
+			'Bearer ' +
+			jwt.sign({ id: newUser.id }, 'SECRET_TOKEN', { expiresIn: '1H' });
+
+		if (!token) {
+			throw new Error(
+				"Désolé, une erreur s'est produite, veuillez réessayer plus tard"
+			);
+		}
+
+		res.status(201).json({ message: 'Utilisateur crée avec succès !' });
 	} catch (error) {
-		next(error);
+		res.status(400).json({ error: error.message });
 	}
 };
 
-exports.readAll = async (req, res, next) => {
-	// Liste tout les employés de la boite
+// Connexion compte utilisateur
+exports.login = async (req, res, next) => {
 	try {
-		const usersdata = await User.findAll();
-		res.send(usersdata);
+		const user = await User.findOne({
+			where: {
+				email: email.req.body,
+			},
+		});
+
+		if (!user) {
+			throw new Error('Compte introuvable');
+		}
+
+		const isValid = await bcrypt.compare(req.body.password, user.password);
+
+		if (!isValid) {
+			throw new Error('Mot de passe incorect');
+		}
+
+		const tokenConnection = jwt.sign(
+			{ id: user.id },
+			process.env.SECRET_TOKEN,
+			{ expiresIn: '1h' }
+		);
+
+		res.status(200).json({
+			id: user.id,
+			tokenConnection,
+		});
+
+		if (!tokenConnection) {
+			throw new Error(
+				"Désolé, une erreur s'est produite, veuillez réessayer plus tard"
+			);
+		}
 	} catch (error) {
-		next(error);
+		res.status(400).json({ error: error.message });
 	}
 };
 
-exports.readOne = async (req, res, next) => {
-	// Affiche le profil d'un employé
+// Affiche profil utilisateur
+exports.findOne = async (req, res, next) => {
 	try {
 		const { id } = req.params;
 		console.log(id);
-		const userData = await User.findOne({ where: { id: id } });
-		res.send(userData);
+		const userFind = await User.findOne({ where: { id: id } });
+		res.status(200).json({ userFind });
 	} catch (error) {
-		next(error);
+		res.status(400).json({ error: error.message });
 	}
 };
 
-exports.update = async (req, res, next) => {
-	// Modification compte employé
+// Mise à jour compte utilisateur
+exports.updateOne = async (req, res, next) => {
 	try {
 		const { body, params } = req;
 		const { id } = params;
-		const updateResData = await User.update(body, { where: { id: id } });
-		res.send({ updateStatus: updateResData });
+		const UpdateUser = await User.update(body, { where: { id: id } });
+		res.status(200).json({
+			user: UpdateUser,
+			message: 'Mise à jour effectué avec succès',
+		});
 	} catch (error) {
-		next(error);
+		res.status(400).json({ error: error.message });
 	}
 };
 
-exports.delete = async (req, res, next) => {
-	// Suppression compte employé
+// Suppression compte utilisateur
+exports.deleteOne = async (req, res, next) => {
 	try {
 		const { id } = req.params;
 		const deleteResult = await User.destroy({ where: { id: id } });
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,14 +4,14 @@ const router = express.Router();
 const userCtrl = require('../controllers/users');
 
 // Création compte employé
-router.post('/register', userCtrl.create);
-// Liste tout les employés de la boite
-router.get('/users=all', userCtrl.readAll);
+router.post('/register', userCtrl.register);
+// Connexion compte employé
+router.post('/login', userCtrl.login);
 // Affiche le profil d'un employé
-router.get('/userprofil=:id', userCtrl.readOne);
+router.get('/userprofil=:id', userCtrl.findOne);
 // Modification compte employé
-router.put('/usermodify:id', userCtrl.update);
+router.put('/usermodify:id', userCtrl.updateOne);
 // Suppression compte employé
-router.delete('/userdelete:id', userCtrl.delete);
+router.delete('/userdelete:id', userCtrl.deleteOne);
 
 module.exports = router;
