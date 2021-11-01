f4156784ef7cc05d90a2e3ce7972a20c9cdb9255
Brian <brian.thiely@gmail.com>
2021-11-01 13:33:11 +0100

    delete sidebar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,15 @@
 <template>
-	<Sidebar />
-	<div :style="{ 'margin-left': sidebarWidth }"></div>
+	
 	<router-view></router-view>
 </template>
 
 <script>
-import Sidebar from '@/components/sidebar/Sidebar';
-import { sidebarWidth } from '@/components/sidebar/state';
 export default {
 	name: 'App',
 	components: {
-		Sidebar,
 	},
 	setup() {
 		return { 
-			sidebarWidth,
-			mode: 'login',
 			};
 	},
 };
